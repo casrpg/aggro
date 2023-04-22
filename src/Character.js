574c44aae4838066b745eeed810d2066e6533426
Andrés Castillo <acastillo.trabajo+git@gmail.com>
2023-04-22 12:31:17 +0200

    Refactor: extract Character class to other file

---
+++ src/Character.js
@@ -0,0 +1,20 @@
+export const defaultEquipment = {ac: 10, carryTwoHandWeapon: false}
+
+export class Character {
+    constructor(name, equipment = defaultEquipment, state) {
+        this.name = name;
+        this.equipment = equipment;
+        this.state = state;
+    }
+    
+    aggroScore() {
+        const STARTING_AGGRO_SCORE = 30;
+        let score = STARTING_AGGRO_SCORE;
+        const {ac, carryTwoHandWeapon} = this.equipment
+        if(carryTwoHandWeapon) {
+            score++
+        }
+        score = score - ac;
+        return score;
+    }
+}
--- src/list-characters.js
+++ src/list-characters.js
@@ -1,25 +1,7 @@
-const STARTING_AGGRO_SCORE = 30;
-const defaultEquipment = {ac: 10, carryTwoHandWeapon: false}
+import { Character, defaultEquipment } from './Character';
+
 const virtualAggroConstants = {advantage: 5/4, disadvantage: 3/4, endurance: 1/2, debility: 2}
 
-
-class Character {
-    constructor(name, equipment = defaultEquipment, state) {
-        this.name = name;
-        this.equipment = equipment;
-        this.state = state;
-    }
-
-    aggroScore() {
-        let score = STARTING_AGGRO_SCORE;
-        const {ac, carryTwoHandWeapon} = this.equipment
-        if(carryTwoHandWeapon) {
-            score++
-        }
-        score = score - ac;
-        return score;
-    }
-}
 
 const legolas = new Character('Legolas',{...defaultEquipment, ac: 14});
 const gimli = new Character('Gimli', {ac: 18, carryTwoHandWeapon: true});
@@ -33,7 +15,7 @@ const maxCharacterNameLength = (characterList) => {
         return Math.max(length, character.name.length)
     }, 0)
     return maxLength;
-} 
+}
 
 const nameColumnLength = maxCharacterNameLength(characters)
 
