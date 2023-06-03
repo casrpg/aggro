2936a2e920626c3bc17f8f7fddb6d9d9af719538
Andrés Castillo <acastillo.trabajo+git@gmail.com>
2023-06-03 12:56:07 +0200

    Implement reset advantage

--- src/Character.js
+++ src/Character.js
@@ -1,26 +1,34 @@
 export const defaultEquipment = {ac: 10, carryTwoHandWeapon: false};
 
+const ADVANTAGE = 'advantage';
+const DISADVANTAGE = 'disAdvantage';
+const NOR_ADVANTAGE_NOR_DISADVANTAGE = 'none';
+
 export class Character {
   constructor(name, equipment = defaultEquipment) {
     this.name = name;
     this.equipment = equipment;
-    this.advantageStatus = null;
+    this.advantageStatus = NOR_ADVANTAGE_NOR_DISADVANTAGE;
   }
 
   giveAdvantage(){
-    this.advantageStatus = 'advantage';
+    this.advantageStatus = ADVANTAGE;
   }
 
   giveDisAdvantage(){
-    this.advantageStatus = 'disAdvantage';
+    this.advantageStatus = DISADVANTAGE;
+  }
+
+  resetAdvantage(){
+    this.advantageStatus = NOR_ADVANTAGE_NOR_DISADVANTAGE;
   }
 
   get advantageFactor() {
-    if(this.advantageStatus === 'advantage'){
+    if(this.advantageStatus === ADVANTAGE){
       return 5/4;
     }
 
-    if (this.advantageStatus === 'disAdvantage'){
+    if (this.advantageStatus === DISADVANTAGE){
       return 3/4;
     }
     return 1;
--- src/Character.spec.js
+++ src/Character.spec.js
@@ -28,16 +28,39 @@ describe('Character', () => {
       expect(character.advantageFactor).toBe(1);
     });
 
-    it('should be 5/4 when character has advantage', () => {
-      character.giveAdvantage();
-
-      expect(character.advantageFactor).toBe(5/4);
-    });
 
     it('should be 3/4 when character has disadvantage', () =>{
       character.giveDisAdvantage();
 
       expect(character.advantageFactor).toBe(3/4);
+    });
+
+    describe('for a character with advantage', () => {
+      beforeEach(() => {
+        character.giveAdvantage();
+      });
+
+      it('should have 5/4 factor', () => {
+        expect(character.advantageFactor).toBe(5/4);
+      });
+
+      it('should not change the factor giving it advantage again', () => {
+        character.giveAdvantage();
+
+        expect(character.advantageFactor).toBe(5/4);
+      });
+
+      it('should have factor 3/4 after giving it disadvantage', () => {
+        character.giveDisAdvantage();
+
+        expect(character.advantageFactor).toBe(3/4);
+      });
+
+      it('should have factor 1 after resetting it', () => {
+        character.resetAdvantage();
+
+        expect(character.advantageFactor).toBe(1);
+      });
     });
   });
 });
